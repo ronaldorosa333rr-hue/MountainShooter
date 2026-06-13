65aec8e88c321fbbebadf16f1a8059babbc39260
naldo <ronaldo-rosa@susepe.rs.go.br>
2026-06-13 14:18:22 -0300

    push

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import pygame 
+from code.Game import Game
 
-print('Setup Start')
-pygame.init()
-windows =pygame.display.set_mode(size=(600, 480))
-print('Setup End')
-
-print('Loop Start')
-while True:
-     # check for all events
-     for event in pygame.event.get():
-         if event.type == pygame.QUIT: 
-            print('Quitting...')  
-            pygame.quit()   # Close Window
-            quit()   # end pygame
-<<<<<<< HEAD
-=======
-      
->>>>>>> c32c3d97d7a1ea39678cecd906506a1b3f319e77
-
+game = Game()
+game.run()
