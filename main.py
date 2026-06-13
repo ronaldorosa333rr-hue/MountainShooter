b534605870e483c5b704b1e7ad96647035670b82
ronaldorosa333rr-hue <ronaldorosa333.rr@gmail.com>
2026-06-13 10:59:30 -0300

    update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ while True:
      # check for all events
      for event in pygame.event.get():
          if event.type == pygame.QUIT: 
+            print('Quitting...')  
             pygame.quit()   # Close Window
             quit()   # end pygame
       
